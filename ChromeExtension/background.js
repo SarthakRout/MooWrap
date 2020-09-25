20cbbfcd1616cf2a0236d4a8e00e6866d993550d
Sarthak Rout <sarthak2024@gmail.com>
2020-09-26 00:03:35 +0530

    Got course names and course urls

--- ChromeExtension/background.js
+++ ChromeExtension/background.js
@@ -1,5 +1,8 @@
-let courses = [];
+let course_names = [];
+let course_urls = [];
 chrome.runtime.onInstalled.addListener(function() {
+	getCourses();
+	chrome.alarms.create('refresh', {periodInMinutes: 2});
     chrome.storage.sync.set({username: 'none'}, function() {
       console.log("No one there.");
     });
@@ -30,3 +33,30 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.cookies.onChanged.addListener(function (cookieChanged) {
 	console.log(cookieChanged.cookie);
 });
+chrome.alarms.onAlarm.addListener(
+	(alarm) => {
+	console.log(alarm.name);
+	updateInfo();
+});
+
+function updateInfo() {
+	console.log("For now, do nothing.");
+}
+
+async function getCourses() {
+	await fetch('https://hello.iitk.ac.in/courses').then( r => r.text()).then(
+	res => {
+		var el = document.createElement('html');
+		el.innerHTML = res;
+		URLs  = (el.getElementsByTagName('a'));
+		for (const item of URLs) {
+			if(item.href.includes('course/')) {
+				const course_url = item.href;
+				course_urls.push(course_url);
+				course_names.push(course_url.substring(32));
+			}
+		}
+		console.log(course_names);
+		console.log(course_urls);
+	});
+}
